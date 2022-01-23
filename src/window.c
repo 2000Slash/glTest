d07a72171a7705e80786fc3185765b1e3c25f648
Nils Hartmann <nils1hartmann@gmail.com>
2022-01-23 12:18:13 +0100

    init sourcecode

---
+++ src/window.c
@@ -0,0 +1,27 @@
+#include "window.h"
+
+GLFWwindow *initialize_window()
+{
+    /* Initialize the library */
+    if (!glfwInit())
+        return NULL;
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return NULL;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        printf("Failed to initialize GLAD");
+        return NULL;
+    }
+
+    glViewport(0, 0, 800, 600);
+    return window;
+}
